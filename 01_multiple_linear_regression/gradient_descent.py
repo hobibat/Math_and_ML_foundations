7492c51bf3031e2fa834e5ebab107ac9880c2023
Habiba Gamal <habibagamal1910@gmail.com>
2026-09-02 03:20:03 +0300

    this is the finalized version for calculating the gradient descent for multiple linear regression with implementation for both numerical and analytical derivative to check that the algorithm is flawless

--- 01_multiple_linear_regression/gradient_descent.py
+++ 01_multiple_linear_regression/gradient_descent.py
@@ -10,28 +10,67 @@ def get_cost(X, y, w, b):
     cost/=2*m
     return cost
 
+
+
+def numeric_gradient(X_train, y_train, w_init, b_init, alpha):
+    eps=1e-7
+
+    w=w_init.copy()
+    b=b_init
+
+    n=len(X_train[0])
+    dj_dw=np.zeros(n)
+    
+    for j in range(n):
+        #i will use the cost function to compute the value of the function obviously
+        og_j= get_cost(X_train, y_train, w, b)
+
+        w[j]+=eps
+        changed_jw= get_cost(X_train, y_train, w, b)
+        dj_dw[j]= (changed_jw - og_j)/eps
+
+        #and don't forget to reset w[j] for the next partial derivative
+        w[j]-=eps
+
+
+    changed_jb= get_cost(X_train, y_train, w, b+eps)
+    dj_db= (changed_jb - get_cost(X_train, y_train, w, b))/eps
+
+    w=w- alpha*dj_dw
+    b=b- alpha*dj_db
+
+    return w, b
+
+#___________________________________________________________________________
+
+def analytical_gradient(X_train, y_train, w_init, b_init, alpha):
+    m=len(X_train)
+    w=w_init.copy()
+    b=b_init
+
+    loss= (np.dot(X_train, w) + b) -y_train
+
+    #now calculate the gradiant
+    dj_dw = (X_train.T @ loss)/m 
+    dj_db = np.sum(loss)/m #its derivative is just the sum of errors as we concluded
+
+    #update the weight and bias
+    w-= alpha*dj_dw
+    b-= alpha*dj_db
+
+    return w, b
+
+
 def gradient_descent(X_train, y_train, w_init, b_init, alpha, iters):
     #so the aim now is that i will compute the gradient for w and b
     #which we concluded that it's the matrix multiplication for the errors vector and transpose of x
     #and i would do this for thousand times and update w
-
-    m=len(X_train)
-    w=w_init
+    w=w_init.copy()
     b=b_init
 
-    #i will first get the errors vector
-    
-
     for epoch in range(iters):
-        loss= (np.dot(X_train, w) + b) -y_train
 
-        #now calculate the gradiant
-        dj_dw = (X_train.T @ loss)/m 
-        dj_db = np.sum(loss)/m #its derivative is just the sum of errors as we concluded
-
-        #update the weight and bias
-        w-= alpha*dj_dw
-        b-= alpha*dj_db
+        w, b= analytical_gradient(X_train, y_train, w, b, alpha)
 
         #for debugging
         if epoch % 100==0:
@@ -42,7 +81,23 @@ def gradient_descent(X_train, y_train, w_init, b_init, alpha, iters):
 
     
 
-    
+def numeric_gradient_descent(X_train, y_train, w_init, b_init, iters):
+    #so the aim now is that i will compute the gradient for w and b
+    #which we concluded that it's the matrix multiplication for the errors vector and transpose of x
+    #and i would do this for thousand times and update w
+    w=w_init.copy()
+    b=b_init
+
+    for epoch in range(iters):
+
+        w, b= numeric_gradient(X_train, y_train, w, b, 0.01)
+
+        #for debugging
+        if epoch % 100==0:
+            cost=get_cost(X_train, y_train, w, b)
+            print(f" cost on {epoch}th iteration is: {cost: .4f}")
+
+    return w, b  
 
 
 
@@ -70,8 +125,16 @@ b_init = np.random.randn()
 
 
 
-w_final, b_final= gradient_descent(X_train, y_train, w_init, b_init, 0.01, 1000)
+w_final, b_final= gradient_descent(X_train, y_train, w_init, b_init, 0.01, 1500)
 
 for i in range(n):
     print(f" weight no.{i+1}: {w_final[i]: .4f}")
 print(f"final bias is: {b_final: .4f}")
+print("\n \n")
+#________________________________________________________________
+
+w_num, b_num= numeric_gradient_descent(X_train, y_train, w_init, b_init, 1500)
+for i in range(n):
+    print(f" weight no.{i+1}: {w_num[i]: .4f}")
+print(f"{b_num: .4f}")
+
